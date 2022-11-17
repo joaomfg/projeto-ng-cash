fe1e2c2bcf86e7dd43910ae6c1fd0cb2409061be
Joaomfg <joaomarcelofg@hotmail.com>
2022-11-17 17:14:01 -0300

    feat: error handler and catalog

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
 import 'express-async-errors';
+import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (_req, res) => res.send('Hello Word'));
+
+app.use(errorHandler);
 
 export default app;
