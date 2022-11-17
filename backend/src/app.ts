e4b4b3127b3f789a2d59d105cde997d1753ca9d3
Joaomfg <joaomarcelofg@hotmail.com>
2022-11-17 17:49:40 -0300

    feat: small details

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import 'express-async-errors';
 import errorHandler from './middlewares/errorHandler';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (_req, res) => res.send('Hello Word'));
 
